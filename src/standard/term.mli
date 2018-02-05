97cfdd62615a4aa1ce75bfe1e29a2dfb1a4bf61d
Guillaume Bury <guillaume.bury@gmail.com>
2018-02-06 00:48:23 +0100

    Added some comments about hspae of terms

--- src/standard/term.mli
+++ src/standard/term.mli
@@ -83,35 +83,57 @@ type builtin =
 
 type binder =
   | All
-  (** Universal quantification *)
+  (** Universal quantification.
+      Each term in the list of quantified terms should represent
+      a variable (optionnally typed using the {Colon} constructor. *)
   | Ex
-  (** Existencial quantification *)
+  (** Existencial quantification
+      Each term in the list of quantified terms should represent
+      a variable (optionnally typed using the {Colon} constructor. *)
   | Pi
-  (** Polymorphic type quantification in function type *)
+  (** Polymorphic type quantification in function type
+      Each term in the list of quantified terms should represent
+      a variable (optionnally typed using the {Colon} constructor. *)
   | Arrow
-  (** The arrow binder, for function types *)
+  (** The arrow binder, for function types. Allows for curified types, if wanted. *)
   | Let
-  (** Let bindings (either propositional or for terms *)
+  (** Let bindings (either propositional or for terms).
+      Term boud by a let can have many forms depending on the language, but usual
+      shapes are:
+      - an equality (using the builtin {Eq}) between a variable
+        (optionnally typed using the {Colon} constructor),
+        and a term (e.g. in tptp)
+      - an equivalence (using the builtin {Equiv}) between a variable
+        (optionnally typed using the {Colon} constructor),
+        and a term/proposition (e.g. in tptp)
+      - a variable and a term juxtaposed using the {Colon} constructor (e.g. in smtlib)
+  *)
   | Fun
-  (* Lambda, i.e function abstraction binder *)
+  (** Lambda, i.e function abstraction binder.
+      Boud terms are the variables bound by the lambda, optionnally typed
+      using the {Colon} constructor. *)
   | Choice
-  (* Indefinite description, or epsilon terms *)
+  (** Indefinite description, or epsilon terms.
+      Likely to have its usual shape change fllowing tptp's recent changes. *)
   | Description
-  (* Definite description *)
+  (** Definite description.
+      Likely to have its usual shape change fllowing tptp's recent changes. *)
 (** The type of binders, these are pretty much always builtin in all languages. *)
 
 type descr =
   | Symbol of Id.t
   (** Constants, variables, etc... any string-identified non-builtin atomic term. *)
   | Builtin of builtin
-  (** Predefined builtins, i.e constants with lexical or syntaxic defintion in the source language. *)
+  (** Predefined builtins, i.e constants with lexical or syntaxic defintion
+      in the source language. *)
   | Colon of t * t
-  (** Juxtaposition of terms, usually used to annotate a term with its type (for quantified variables,
-      functions arguments, etc... *)
+  (** Juxtaposition of terms, usually used to annotate a term with its type
+      (for quantified variables, functions arguments, etc...). *)
   | App of t * t list
   (** Higher-order application *)
   | Binder of binder * t list * t
-  (** Binder (quantifiers, local functions, ...) *)
+  (** Binder (quantifiers, local functions, ...), see the {!binder} type for more
+      information. *)
   | Match of t * (t * t) list
   (** Pattern matching, the list contains tuples of the form [(pattern,branch)]. *)
 (** The AST for terms *)
