f3e513a7dca2e1f51bde6935c8c9991c47d91b61
Guillaume Bury <guillaume.bury@gmail.com>
2020-03-11 16:40:55 +0100

    Add definitions by substitutions

--- src/typecheck/def.ml
+++ src/typecheck/def.ml
@@ -4,15 +4,77 @@ open Dolmen
 (* Definitions by Substitution *)
 (* ************************************************************************ *)
 
-module type Expand_arg = sig
+module type Subst_arg = sig
 
-  type t
+  type ty
+  type ty_var
+
+  type term
+  type term_var
+
+  val ty_subst :
+    (ty_var * ty) list -> ty -> ty
+  val term_subst :
+    (ty_var * ty) list -> (term_var * term) list -> term -> term
 
 end
 
-module Expand
+module Subst
     (Type : Tff_intf.S)
-    (T : Expand_arg with type t = Type.T.t) = struct
+    (T : Subst_arg with type ty = Type.Ty.t
+                    and type ty_var = Type.Ty.Var.t
+                    and type term = Type.T.t
+                    and type term_var = Type.T.Var.t) = struct
+
+  module H = Hashtbl.Make(Id)
+
+  let take_drop n l =
+    let rec aux acc n = function
+      | r when n <= 0 -> List.rev acc, r
+      | [] -> raise (Invalid_argument "take_drop")
+      | x :: r -> aux (x :: acc) (n - 1) r
+    in
+    aux [] n l
+
+  let definitions = H.create 13
+
+  let define_ty id vars body =
+    H.add definitions id (`Ty (vars, body))
+
+  let define_term id vars args body =
+    H.add definitions id (`Term (vars, args, body))
+
+  let parse env ast symbol args =
+    match (symbol : Type.symbol) with
+    | Id id ->
+      begin match H.find definitions id with
+        | `Ty (vars, body) ->
+          Base.make_opn (List.length vars)
+            (module Type) env ast (Id.full_name id) args (fun args ->
+                let ty_args = List.map (Type.parse_ty env) args in
+                let l = List.map2 (fun x y -> x, y) vars ty_args in
+                Type.Ty (T.ty_subst l body)
+              )
+        | `Term (ty_vars, t_vars, body) ->
+          let n_args = List.length args in
+          let n_ty = List.length ty_vars in
+          let n_t = List.length t_vars in
+          let ty_l, t_l =
+            if n_args = n_ty + n_t then
+              take_drop n_ty args
+            else begin
+              let err = Type.Bad_op_arity (Id.full_name id, n_ty, n_ty) in
+              raise (Type.Typing_error (err, env, ast))
+            end
+          in
+          let ty_l = List.map2 (fun x y -> x, y) ty_vars
+              (List.map (Type.parse_ty env) ty_l) in
+          let t_l = List.map2 (fun x y -> x, y) t_vars
+              (List.map (Type.parse_term env) t_l) in
+          Some (Type.Term (T.term_subst ty_l t_l body))
+        | exception Not_found -> None
+      end
+    | Builtin _ -> None
 
 end
 
--- src/typecheck/def.mli
+++ src/typecheck/def.mli
@@ -4,8 +4,49 @@
     Helpful helpers to handle defined symbols.
 *)
 
+
+(** {2 Definitions as declarations} *)
+
 (** Handle definitions as declaring new constants. *)
 module Declare(Type : Tff_intf.S) : sig
+
+  val define_ty :
+    Dolmen.Id.t -> Type.Ty.Var.t list -> Type.Ty.t -> unit
+  (** Define a type constant. *)
+
+  val define_term :
+    Dolmen.Id.t -> Type.Ty.Var.t list -> Type.T.Var.t list -> Type.T.t -> unit
+  (** Define a term constant. *)
+
+  val parse : Type.builtin_symbols
+  (** Adequate builtin symbols function for constants defined/declared. *)
+
+end
+
+(** {2 Definitions using substitution} *)
+
+(** Signature for substitution functions over types and terms *)
+module type Subst_arg = sig
+
+  type ty
+  type ty_var
+
+  type term
+  type term_var
+
+  val ty_subst :
+    (ty_var * ty) list -> ty -> ty
+  val term_subst :
+    (ty_var * ty) list -> (term_var * term) list -> term -> term
+
+end
+
+(** Handle definitions by subsituting in the bodies during type-checking. *)
+module Subst(Type : Tff_intf.S)
+    (T : Subst_arg with type ty = Type.Ty.t
+                    and type ty_var = Type.Ty.Var.t
+                    and type term = Type.T.t
+                    and type term_var = Type.T.Var.t) : sig
 
   val define_ty :
     Dolmen.Id.t -> Type.Ty.Var.t list -> Type.Ty.t -> unit
