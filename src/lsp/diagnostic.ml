98cd6c685b3b6dd28f6fa9cca071988516b20fdb
Guillaume Bury <guillaume.bury@gmail.com>
2020-10-22 13:09:59 +0200

    Update code to version 1.1.0 of lsp

--- src/lsp/diagnostic.ml
+++ src/lsp/diagnostic.ml
@@ -1,13 +1,13 @@
 
 (* This file is free software, part of dolmen. See file "LICENSE" for more information *)
 
-type t = Lsp.Protocol.PublishDiagnostics.diagnostic
+type t = Lsp.Types.Diagnostic.t
 
 let lsp_pos line character =
-  Lsp.Protocol.Position.{ line; character; }
+  Lsp.Types.Position.create ~line ~character
 
-let lsp_range start_ end_ =
-  Lsp.Protocol.Range.{ start_; end_; }
+let lsp_range start end_ =
+  Lsp.Types.Range.create ~start ~end_
 
 let start_pos = lsp_pos 1 1
 let start_range = lsp_range start_pos start_pos
@@ -19,26 +19,18 @@ let range_of_loc = function
       (lsp_pos (l.start_line - 1) l.start_column)
       (lsp_pos (l.stop_line - 1) l.stop_column)
 
-let warn ?loc msg =
-  Lsp.Protocol.PublishDiagnostics.{
-    range = range_of_loc loc;
-    severity = Some Warning;
-    code = NoCode;
-    source = Some "dolmenls";
-    message = msg;
-    relatedInformation = [];
-    tags = [];
-  }
+let warn ?loc message =
+  Lsp.Types.Diagnostic.create ()
+    ~range:(range_of_loc loc)
+    ~severity:Warning
+    ~source:"dolmenls"
+    ~message
 
-let error ?loc msg =
-  Lsp.Protocol.PublishDiagnostics.{
-    range = range_of_loc loc;
-    severity = Some Error;
-    code = NoCode;
-    source = Some "dolmenls";
-    message = msg;
-    relatedInformation = [];
-    tags = [];
-  }
+let error ?loc message =
+  Lsp.Types.Diagnostic.create ()
+    ~range:(range_of_loc loc)
+    ~severity:Error
+    ~source:"dolmenls"
+    ~message
 
 
