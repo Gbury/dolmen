f4b0e52e7ec4071adcbba743499138136168bb70
Guillaume Bury <guillaume.bury@gmail.com>
2021-02-17 00:14:19 +0100

    Switch to using Linol

--- src/lsp/main.ml
+++ src/lsp/main.ml
@@ -1,58 +1,32 @@
 
 (* This file is free software, part of dolmen. See file "LICENSE" for more information *)
 
-type listen =
-  | Stdio
-  | Server of string * int
+module Task = Linol_lwt.Task
 
-let run ic oc =
-  let section = Printf.sprintf "%s[%d]" Handler.section @@ Thread.id (Thread.self ()) in
-  (* Lsp server setup *)
-  let io = Lsp.Io.make ic oc in
-  let scheduler = Lsp.Scheduler.create () in
-  let stream_io = Lsp.Rpc.Stream_io.make scheduler io in
-  let server = Lsp.Server.make Handler.handler stream_io Handler.empty in
-  (* Start the server *)
-  Lsp.Logger.log ~section ~title:Debug "start lsp";
-  Lsp.Scheduler.run scheduler (Lsp.Server.start server)
-
-let main_tcp addr port =
-  let sock = Unix.socket ~cloexec:true Unix.PF_INET Unix.SOCK_STREAM 0 in
-  Unix.setsockopt_optint sock Unix.SO_LINGER None;
-  Unix.setsockopt sock Unix.SO_REUSEADDR true;
-  let inet_addr = Unix.inet_addr_of_string addr in
-  Unix.bind sock (Unix.ADDR_INET (inet_addr, port));
-  Unix.listen sock 10;
-  while true do
-    let client_sock, _ = Unix.accept sock in
-    let ic = Unix.in_channel_of_descr client_sock in
-    let oc = Unix.out_channel_of_descr client_sock in
-    let _th = Thread.create (fun () -> run ic oc) () in
-    ()
-  done;
-  ()
-
-let main ~listen () : unit =
-  begin match listen with
-    | Stdio -> run stdin stdout
-    | Server (addr,port) ->
-      main_tcp addr port
-  end
+let run () =
+  let open Task.Infix in
+  let s = new Server.dolmen_lsp_server in
+  (* TODO: the task is the LSP server *)
+  let task =
+    Task.start ~descr:"top task"
+      (fun _top_task ->
+         let server = Linol_lwt.Jsonrpc2.create_stdio s in
+         let* () =
+           Task.run_sub ~descr:"lsp server" ~parent:_top_task
+             (fun _ -> Linol_lwt.Jsonrpc2.run server _top_task)
+           >>= Task.unwrap
+         in
+         Task.return ()
+      )
+  in
+  match Task.run task with
+  | Ok () -> 0
+  | Error e ->
+    let e = Printexc.to_string e in
+    Printf.eprintf "error: %s\n%!" e;
+    1
 
 let () =
-  let stdio = ref true in
-  let host = ref "0.0.0.0" in
-  let port = ref 8854 in
-  let logfile = ref "/tmp/dolmenls.log" in
-  let opts = [
-    "--host", Arg.Set_string host, " address to listen in";
-    "--port", Arg.Set_int port, " port to listen on";
-    "--stdio", Arg.Set stdio, " connection on stdio";
-    "--tcp", Arg.Clear stdio, " connections on TCP";
-    "--log", Arg.Set_string logfile, " log file";
-  ] |> Arg.align in
-  Arg.parse opts (fun _ -> raise (Arg.Bad "no such arg")) "dolmenls [option*]";
-  if !logfile = "" then logfile := Filename.temp_file "dolmenls-" ".log";
-  let listen = if !stdio then Stdio else Server (!host, !port) in
-  Lsp.Logger.with_log_file (Some !logfile) (main ~listen)
+  let retcode = run () in
+  exit retcode
 
---
+++ src/lsp/server.ml
@@ -0,0 +1,41 @@
+(* This file is free software, part of dolmen. See file "LICENSE" for more information *)
+
+(* Lsp server class *)
+(* ************************************************************************ *)
+
+class dolmen_lsp_server =
+  object(self)
+    inherit Linol_lwt.Jsonrpc2.server
+
+    (* one env per document *)
+    val buffers: (Lsp.Types.DocumentUri.t, State.t) Hashtbl.t = Hashtbl.create 32
+
+    method private _on_doc
+        ~(notify_back:Linol_lwt.Jsonrpc2.notify_back)
+        (uri:Lsp.Types.DocumentUri.t) (contents:string) =
+      (* TODO: unescape uri/translate it to a correct path ? *)
+      match Loop.process uri (Some contents) with
+      | Ok state ->
+        let diags = state.solve_state in
+        Hashtbl.replace buffers uri state;
+        notify_back#send_diagnostic diags
+      | Error msg ->
+        Linol_lwt.Jsonrpc2.IO.failwith (
+          Format.asprintf "Internal dolmen error: %s" msg
+        )
+
+    method on_notif_doc_did_open ~notify_back d ~content : unit Linol_lwt.Task.m =
+      self#_on_doc ~notify_back d.uri content
+
+    method on_notif_doc_did_close ~notify_back:_ d : unit Linol_lwt.Task.m =
+      Hashtbl.remove buffers d.uri;
+      Linol_lwt.Jsonrpc2.IO.return ()
+
+    method on_notif_doc_did_change ~notify_back d _c ~old_content:_old ~new_content =
+      self#_on_doc ~notify_back d.uri new_content
+
+  end
+
+
+
+
