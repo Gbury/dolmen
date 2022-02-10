0e7a11419b347dc4a3e6873c0ff4af87e866e984
Guillaume Bury <guillaume.bury@gmail.com>
2022-02-10 16:15:54 +0100

    Some rearranging of mangpage doc

--- src/bin/man.ml
+++ src/bin/man.ml
@@ -10,13 +10,15 @@ let cli = [
       used in automated deduction.";
   `S Options.common_section;
   `P "Common options for the dolmen binary";
+  `S Options.model_section;
+  `P "Options to use dolmen to verify models output by provers";
+  `S Options.header_section;
+  `P "Options to control the checking of headers in the input file";
   `S Options.error_section;
   `P "Options to customize the behaviour of dolmen on errors/warnings";
   `P "A warning can have one of three status: Disabled, Enabled, and Fatal. \
       When disabled, a warning will be ignored, when enabled, it will be
         printed, and when fatal, it will be transformed into an error.";
-  `S Options.header_section;
-  `P "Options to control the checking of headers in the input file";
   `S Options.profiling_section;
   `P (Format.asprintf
         "Options to profile Dolmen.%s"
