f1d8e041c629b7889b43b5f53a186a466ef6418d
Guillaume Bury <guillaume.bury@gmail.com>
2023-02-16 15:17:12 +0100

    Add hints and tests for interleaver mode failure

--- src/bin/hints.ml
+++ src/bin/hints.ml
@@ -8,6 +8,13 @@ let model ~check_model ~check_model_mode =
         (Format.dprintf "%a" Format.pp_print_text
            "The full mode for checking model can use up a lot of memory, \
             you might want to try using the `--check-model-mode=interleave` option.");
+  if check_model && (check_model_mode = Dolmen_model.Loop.Interleave) then
+    Dolmen_loop.Report.add_hint Dolmen_model.Loop.undefined_constant
+      (Format.dprintf "%a" Format.pp_print_text
+         "The interleave mode for checking model requires constants in the \
+          model file to be defined in the same order as they are declared in \
+          the input problem file, \
+          you might want to try using the `--check-model-mode=full` option.");
   ()
 
 
