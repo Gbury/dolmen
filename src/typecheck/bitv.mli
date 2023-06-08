8ff8adc284786b949668ddc678c400a05ea2a4b0
Hichem R. A <hra687261@gmail.com>
2023-06-08 15:03:58 +0200

    Update src/typecheck/bitv.mli
    Co-authored-by: Guillaume Bury <guillaume.bury@gmail.com>

--- src/typecheck/bitv.mli
+++ src/typecheck/bitv.mli
@@ -12,7 +12,7 @@ module Ae : sig
     type _ Type.err +=
       | Invalid_bin_char : char -> Dolmen.Std.Term.t Type.err
       (** Error raised when a character that isn't '0' or '1' is used inside a
-          literal bitvector. *)
+          literal bitvector in binary form. *)
       | Expected_nat_lit : Dolmen.Std.Term.t -> Dolmen.Std.Term.t Type.err
       (** Error raised when a function that expects a literal natural number
           as an argument receives something else. *)
